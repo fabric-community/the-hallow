a3eaa82d30aec384d2a82c052c6733fc6f9ca2e8
Yanis48 <doublecraft.official@gmail.com>
2019-10-01 22:21:06 +0200

    added the pumpcown

---
+++ src/main/java/com/fabriccommunity/spookytime/client/PumpcownEntityRenderer.java
@@ -0,0 +1,24 @@
+package com.fabriccommunity.spookytime.client;
+
+import com.fabriccommunity.spookytime.SpookyTime;
+import com.fabriccommunity.spookytime.entity.PumpcownEntity;
+
+import net.minecraft.client.render.entity.EntityRenderDispatcher;
+import net.minecraft.client.render.entity.MobEntityRenderer;
+import net.minecraft.client.render.entity.model.CowEntityModel;
+import net.minecraft.util.Identifier;
+
+@SuppressWarnings({"rawtypes", "unchecked"})
+public class PumpcownEntityRenderer extends MobEntityRenderer<PumpcownEntity, CowEntityModel<PumpcownEntity>> {
+	private static final Identifier SKIN = new Identifier(SpookyTime.MODID, "textures/entity/pumpcown.png");
+	
+	public PumpcownEntityRenderer(EntityRenderDispatcher dispatcher) {
+		super(dispatcher, new CowEntityModel(), 0.7F);
+		this.addFeature(new PumpcownStemFeatureRenderer(this));
+	}
+
+	@Override
+	protected Identifier getTexture(PumpcownEntity pumpcown) {
+		return SKIN;
+	}
+}
