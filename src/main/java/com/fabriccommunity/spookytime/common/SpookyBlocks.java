f56942334c678d93133215c1cc619391d1284f13
vini2003 <vini.sch@outlook.com>
2019-10-01 15:11:00 -0300

    Add Tiny Pumpkin to block registry.

--- src/main/java/com/fabriccommunity/spookytime/common/SpookyBlocks.java
+++ src/main/java/com/fabriccommunity/spookytime/common/SpookyBlocks.java
@@ -1,7 +1,12 @@
 package com.fabriccommunity.spookytime.common;
 
 import com.fabriccommunity.spookytime.SpookyTime;
+import com.fabriccommunity.spookytime.common.block.TinyPumpkinBlock;
+
+import net.fabricmc.fabric.api.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -9,7 +14,10 @@ public class SpookyBlocks
 {
 
 
+    public static Block TINY_PUMPKIN;
+
     public static void init() {
+        register("tiny_pumpkin", TINY_PUMPKIN = new TinyPumpkinBlock(FabricBlockSettings.of(Material.PUMPKIN).strength(1.0F, 1.0F).sounds(BlockSoundGroup.LANTERN).build())); 
         // NO-OP
     }
 
