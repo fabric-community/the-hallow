580ee278a4bc017c54c83b99676b6afc2f2107b9
Indigo Amann <indigoa.coding@gmail.com>
2019-10-02 09:01:42 -0700

    Remove @Override injector

--- src/main/java/com/fabriccommunity/spookytime/mixin/EndermanPumpkinMixin.java
+++ src/main/java/com/fabriccommunity/spookytime/mixin/EndermanPumpkinMixin.java
@@ -1,34 +1,28 @@
 package com.fabriccommunity.spookytime.mixin;
 
 import com.fabriccommunity.spookytime.MixinHelpers;
-import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.EntityData;
-import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnType;
 import net.minecraft.entity.mob.EndermanEntity;
-import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.LocalDifficulty;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /**
  * @author Indigo Amann
  */
-@Mixin(EndermanEntity.class)
-public abstract class EndermanPumpkinMixin extends HostileEntity {
-    @Shadow public abstract void setCarriedBlock(BlockState blockState_1);
+@Mixin(MobEntity.class)
+public abstract class EndermanPumpkinMixin {
 
-    protected EndermanPumpkinMixin(EntityType<? extends HostileEntity> entityType_1, World world_1) {
-        super(entityType_1, world_1);
-    }
-
-    @Override
-    public EntityData initialize(IWorld iWorld, LocalDifficulty localDifficulty, SpawnType spawnType, EntityData entityData, CompoundTag compoundTag) {
-        if (MixinHelpers.RANDOM.nextInt(10) == 0) this.setCarriedBlock((MixinHelpers.RANDOM.nextBoolean() ? Blocks.PUMPKIN : (MixinHelpers.RANDOM.nextBoolean() ? Blocks.CARVED_PUMPKIN : Blocks.JACK_O_LANTERN)).getDefaultState());
-        return super.initialize(iWorld, localDifficulty, spawnType, entityData, compoundTag);
+    @Inject(method = "initialize", at = @At("RETURN"))
+    public void initialize(IWorld iWorld, LocalDifficulty localDifficulty, SpawnType spawnType, EntityData entityData, CompoundTag compoundTag, CallbackInfoReturnable<EntityData> cir) {
+        if (((Object)this) instanceof EndermanEntity && MixinHelpers.RANDOM.nextInt(10) == 0) ((EndermanEntity)(Object)this).setCarriedBlock((MixinHelpers.RANDOM.nextBoolean() ? Blocks.PUMPKIN : (MixinHelpers.RANDOM.nextBoolean() ? Blocks.CARVED_PUMPKIN : Blocks.JACK_O_LANTERN)).getDefaultState());
     }
 }
