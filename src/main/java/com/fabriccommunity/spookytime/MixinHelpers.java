91e918d5e05c10ffa4b1c93b3b0f285bb9e681d9
Indigo Amann <indigoa.coding@gmail.com>
2019-10-01 17:52:32 -0700

    Mixin helper so i dont have to retype the whole thing for every mob

---
+++ src/main/java/com/fabriccommunity/spookytime/MixinHelpers.java
@@ -0,0 +1,26 @@
+package com.fabriccommunity.spookytime;
+
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+
+import java.util.Random;
+
+/**
+ * @author Indigo Amann
+ */
+public class MixinHelpers {
+    public static final Random random = new Random();
+    public static ItemStack getEquippedPumpkin(LivingEntity entity, EquipmentSlot slot) {
+        if (slot != EquipmentSlot.HEAD) return entity.getEquippedStack(slot);
+        ItemStack stack = entity.getEquippedStack(EquipmentSlot.HEAD);
+        if (stack.isEmpty()) {
+            if (random.nextInt(3) == 0) {
+                stack = new ItemStack(random.nextBoolean() ? Items.CARVED_PUMPKIN : Items.JACK_O_LANTERN);
+                entity.setEquippedStack(EquipmentSlot.HEAD, stack);
+            }
+        }
+        return stack;
+    }
+}
--- src/main/java/com/fabriccommunity/spookytime/mixin/ZombiePumpkinMixin.java
+++ src/main/java/com/fabriccommunity/spookytime/mixin/ZombiePumpkinMixin.java
@@ -1,6 +1,9 @@
 package com.fabriccommunity.spookytime.mixin;
 
+import com.fabriccommunity.spookytime.MixinHelpers;
+import com.sun.org.apache.bcel.internal.generic.I2F;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.mob.SkeletonEntity;
 import net.minecraft.entity.mob.ZombieEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
@@ -14,17 +17,9 @@ import java.util.Random;
  * @author Indigo Amann
  */
 @Mixin(ZombieEntity.class)
-public class MobPumpkinMixin {
+public class ZombiePumpkinMixin {
     @Redirect(method = "initialize", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/mob/ZombieEntity;getEquippedStack(Lnet/minecraft/entity/EquipmentSlot;)Lnet/minecraft/item/ItemStack;"))
     private ItemStack getStack(ZombieEntity zombieEntity, EquipmentSlot equipmentSlot_1) {
-        ItemStack stack = zombieEntity.getEquippedStack(EquipmentSlot.HEAD);
-        if (stack.isEmpty()) {
-            Random random = new Random();
-            if (random.nextInt(3) == 0) {
-                stack = new ItemStack(random.nextBoolean() ? Items.CARVED_PUMPKIN : Items.JACK_O_LANTERN);
-                zombieEntity.setEquippedStack(EquipmentSlot.HEAD, stack);
-            }
-        }
-        return stack;
+        return MixinHelpers.getEquippedPumpkin(zombieEntity, equipmentSlot_1);
     }
 }
