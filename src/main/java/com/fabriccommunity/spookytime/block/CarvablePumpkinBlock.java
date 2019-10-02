3c1e472384ee0630c938efb71fd83915a6d05238
Indigo Amann <indigoa.coding@gmail.com>
2019-10-02 13:31:43 -0700

    Random carving

--- src/main/java/com/fabriccommunity/spookytime/block/CarvablePumpkinBlock.java
+++ src/main/java/com/fabriccommunity/spookytime/block/CarvablePumpkinBlock.java
@@ -4,8 +4,13 @@ import com.fabriccommunity.spookytime.block.entity.CarvablePumpkinBlockEntity;
 import net.fabricmc.fabric.api.block.FabricBlockSettings;
 import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 
 /**
  * @author Indigo Amann
@@ -20,6 +25,14 @@ public class CarvablePumpkinBlock extends BlockWithEntity {
         return new CarvablePumpkinBlockEntity();
     }
     public boolean isOpaque(BlockState blockState_1) {
+        return false;
+    }
+
+    public boolean activate(BlockState blockState_1, World world_1, BlockPos blockPos_1, PlayerEntity playerEntity_1, Hand hand_1, BlockHitResult blockHitResult_1) {
+        BlockEntity blockEntity_1 = world_1.getBlockEntity(blockPos_1);
+        if (blockEntity_1 instanceof CarvablePumpkinBlockEntity) {
+            return ((CarvablePumpkinBlockEntity) blockEntity_1).activate(blockState_1, world_1, blockPos_1, playerEntity_1, hand_1, blockHitResult_1);
+        }
         return false;
     }
 }
--- src/main/java/com/fabriccommunity/spookytime/block/entity/CarvablePumpkinBlockEntity.java
+++ src/main/java/com/fabriccommunity/spookytime/block/entity/CarvablePumpkinBlockEntity.java
@@ -1,8 +1,20 @@
 package com.fabriccommunity.spookytime.block.entity;
 
 import com.fabriccommunity.spookytime.common.SpookyBlocks;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.ByteTag;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import java.util.Arrays;
+import java.util.Random;
 
 /**
  * @author Indigo Amann
@@ -10,5 +22,32 @@ import net.minecraft.block.entity.BlockEntityType;
 public class CarvablePumpkinBlockEntity extends BlockEntity {
     public CarvablePumpkinBlockEntity() {
         super(SpookyBlocks.CARVABLE_PUMPKIN_BLOCK_ENTITY);
+    }
+    //                                      Face  X   Y
+    public boolean[][][] carving = new boolean[4][16][16];
+
+    @Override
+    public CompoundTag toTag(CompoundTag tag) {
+        super.toTag(tag);
+        ListTag faces = new ListTag();
+        for (boolean[][] carvingFace : carving) {
+            ListTag face = new ListTag();
+            for (boolean[] carvingRow: carvingFace) {
+                ListTag row = new ListTag();
+                for (boolean carved: carvingRow) {
+                    row.add(new ByteTag((byte) (carved ? 1 : 0)));
+                }
+                face.add(row);
+            }
+            faces.add(face);
+        }
+        tag.put("carving", faces);
+        return tag;
+    }
+    public boolean activate(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hitResult) {
+        world.rayTrace()
+        Random random = new Random();
+        carving[random.nextInt(4)][random.nextInt(16)][random.nextInt(16)] = true;
+        return true;
     }
 }
