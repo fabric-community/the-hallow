1548f7d84d2376a3c1568cd70902b65e1654272c
Virtuoel <Virtuoel@users.noreply.github.com>
2019-10-03 19:16:47 +0300

    Waterloggable Tiny Pumpkin

--- src/main/java/com/fabriccommunity/spookytime/block/TinyPumpkinBlock.java
+++ src/main/java/com/fabriccommunity/spookytime/block/TinyPumpkinBlock.java
@@ -4,15 +4,20 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockRenderLayer;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalFacingBlock;
+import net.minecraft.block.Waterloggable;
 import net.minecraft.entity.EntityContext;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateFactory;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
-import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.IWorld;
 
-public class TinyPumpkinBlock extends HorizontalFacingBlock {
+public class TinyPumpkinBlock extends HorizontalFacingBlock implements Waterloggable {
 	protected static final VoxelShape Y_SHAPE = Block.createCuboidShape(6.0D, 0.0D, 6.0D, 10.0D, 8.0D, 10.0D);
 	
 	public TinyPumpkinBlock(Settings blockSettings) {
@@ -21,18 +26,34 @@ public class TinyPumpkinBlock extends HorizontalFacingBlock {
 	
 	@Override
 	public BlockState getPlacementState(ItemPlacementContext placementContext) {
-		return this.getDefaultState().with(FACING, placementContext.getPlayerFacing().getOpposite());
+		final BlockState blockState = this.getDefaultState().with(FACING, placementContext.getPlayerFacing().getOpposite());
+		if (blockState.contains(Properties.WATERLOGGED))
+		{
+			final FluidState fluidState = placementContext.getWorld().getFluidState(placementContext.getBlockPos());
+			return blockState.with(Properties.WATERLOGGED, fluidState.getFluid() == Fluids.WATER);
+		}
+		
+		return blockState;
 	}
 	
+	@Override
+	public FluidState getFluidState(BlockState blockState) {
+		return blockState.contains(Properties.WATERLOGGED) && blockState.get(Properties.WATERLOGGED) ? Fluids.WATER.getStill(false) : super.getFluidState(blockState);
+	}
+	
+	@Override
+	public BlockState getStateForNeighborUpdate(BlockState blockState, Direction direction, BlockState blockStateOther, IWorld world, BlockPos blockPosition, BlockPos blockPositionOther) {
+		if (blockState.contains(Properties.WATERLOGGED) && blockState.get(Properties.WATERLOGGED))
+		{
+			world.getFluidTickScheduler().schedule(blockPosition, Fluids.WATER, Fluids.WATER.getTickRate(world));
+		}
+		
+		return super.getStateForNeighborUpdate(blockState, direction, blockStateOther, world, blockPosition, blockPosition);
+	}
 	
 	@Override
 	public VoxelShape getOutlineShape(BlockState blockState, BlockView blockView, BlockPos blockPosition, EntityContext entityContext) {
 		return Y_SHAPE;
-	}
-	
-	@Override
-	public VoxelShape getCollisionShape(BlockState blockState, BlockView blockView, BlockPos blockPosition, EntityContext entityContext) {
-		return this.collidable ? blockState.getOutlineShape(blockView, blockPosition) : VoxelShapes.empty();
 	}
 	
 	@Override
@@ -42,6 +63,6 @@ public class TinyPumpkinBlock extends HorizontalFacingBlock {
 	
 	@Override
 	protected void appendProperties(StateFactory.Builder<Block, BlockState> builder) {
-		builder.add(HorizontalFacingBlock.FACING);
+		builder.add(HorizontalFacingBlock.FACING, Properties.WATERLOGGED);
 	}
 }
