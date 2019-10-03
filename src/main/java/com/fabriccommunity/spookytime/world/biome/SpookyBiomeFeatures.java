5374a3e8d444dab44cd2c61401f802c5e93c27e3
Indigo Amann <indigoa.coding@gmail.com>
2019-10-02 22:04:36 -0700

    For some reason this works and dosen't change the floor block. Idk why but it works.

--- src/main/java/com/fabriccommunity/spookytime/world/biome/SpookyBiomeFeatures.java
+++ src/main/java/com/fabriccommunity/spookytime/world/biome/SpookyBiomeFeatures.java
@@ -4,17 +4,22 @@ import com.fabriccommunity.spookytime.registry.SpookyBlocks;
 import com.google.common.collect.Lists;
 
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.decorator.CountDecoratorConfig;
+import net.minecraft.world.gen.decorator.CountExtraChanceDecoratorConfig;
 import net.minecraft.world.gen.decorator.Decorator;
 import net.minecraft.world.gen.feature.DiskFeatureConfig;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.FeatureConfig;
 import net.minecraft.world.gen.surfacebuilder.TernarySurfaceConfig;
 
 public class SpookyBiomeFeatures {
-	public static TernarySurfaceConfig SPOOKY_FOREST = new TernarySurfaceConfig(SpookyBlocks.DECEASED_GRASS_BLOCK.getDefaultState(), SpookyBlocks.DECEASED_DIRT.getDefaultState(), SpookyBlocks.TAINTED_GRAVEL.getDefaultState());
+	public static TernarySurfaceConfig SPOOKY_FOREST = new TernarySurfaceConfig(/*SpookyBlocks.DECEASED_GRASS_BLOCK.getDefaultState()*/Blocks.GRASS_BLOCK.getDefaultState(), SpookyBlocks.DECEASED_DIRT.getDefaultState(), SpookyBlocks.TAINTED_GRAVEL.getDefaultState());
 	
 	public static void addSpookyForestTrees(Biome biome) {
+		biome.addFeature(GenerationStep.Feature.VEGETAL_DECORATION, Biome.configureFeature(Feature.DARK_OAK_TREE, FeatureConfig.DEFAULT, Decorator.COUNT_EXTRA_HEIGHTMAP, new CountExtraChanceDecoratorConfig(12, 0.1F, 1)));
 		//Still needs work
 	}
 	
