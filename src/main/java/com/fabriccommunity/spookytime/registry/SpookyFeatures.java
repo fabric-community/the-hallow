ed46828c9beb9ac83bbc17fe05dcbd20ff8921ed
Indigo Amann <indigoa.coding@gmail.com>
2019-10-03 10:11:27 -0700

    Seperate lines

--- src/main/java/com/fabriccommunity/spookytime/registry/SpookyFeatures.java
+++ src/main/java/com/fabriccommunity/spookytime/registry/SpookyFeatures.java
@@ -12,9 +12,8 @@ import net.minecraft.world.gen.feature.FeatureConfig;
  * @author Indigo Amann
  */
 public class SpookyFeatures {
-    public static Feature<DefaultFeatureConfig>
-            SMALL_SKELETON_TREE = register("skeleton_tree_small", new SmallSkeletalTreeFeature(DefaultFeatureConfig::deserialize, false)),
-            LARGE_SKELETON_TREE = register("skeleton_tree_large", new LargeSkeletalTreeFeature(DefaultFeatureConfig::deserialize, false));
+    public static Feature<DefaultFeatureConfig> SMALL_SKELETON_TREE = register("skeleton_tree_small", new SmallSkeletalTreeFeature(DefaultFeatureConfig::deserialize, false));
+    public static Feature<DefaultFeatureConfig> LARGE_SKELETON_TREE = register("skeleton_tree_large", new LargeSkeletalTreeFeature(DefaultFeatureConfig::deserialize, false));
     public static void init() {
 
     }
