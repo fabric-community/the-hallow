6e56b3f476e107c412264d9ea8952b9552efcddb
vini2003 <vini.sch@outlook.com>
2019-10-03 15:29:48 -0300

    Remove trailing newlines.

--- src/main/java/com/fabriccommunity/spookytime/world/layer/SetBiomeGroupsLayer.java
+++ src/main/java/com/fabriccommunity/spookytime/world/layer/SetBiomeGroupsLayer.java
@@ -7,7 +7,7 @@ import net.minecraft.world.biome.layer.LayerRandomnessSource;
 
 public enum SetBiomeGroupsLayer implements InitLayer {
 	INSTANCE;
-	
+
 	@Override
 	public int sample(LayerRandomnessSource rand, int x, int z) {
 		int result = SpookyBiomeGroup.pickRandomBiomeGroup(rand);
