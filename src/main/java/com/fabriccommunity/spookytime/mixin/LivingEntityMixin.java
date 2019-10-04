f392b3e91e2016f4ee9bf262a5b2f19879c7740d
vini2003 <vini.sch@outlook.com>
2019-10-04 16:59:08 -0300

    Fix NPE related to Beheading.

--- src/main/java/com/fabriccommunity/spookytime/mixin/LivingEntityMixin.java
+++ src/main/java/com/fabriccommunity/spookytime/mixin/LivingEntityMixin.java
@@ -23,29 +23,29 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class LivingEntityMixin {
 	@Inject(method = "drop", at = @At("HEAD"))
 	public void drop(DamageSource damageSource, CallbackInfo info) {
-		LivingEntity livingEntity = (LivingEntity) (Object) this;
-		if (livingEntity.world.getGameRules().getBoolean(GameRules.DO_MOB_LOOT) && BeheadingEnchantment.hasBeheading((LivingEntity) damageSource.getAttacker())) {
-			if (BeheadingEnchantment.getHead(damageSource)) {
-				if (livingEntity.getType() == EntityType.WITHER_SKELETON) {
-					livingEntity.dropStack(new ItemStack(Items.WITHER_SKELETON_SKULL));
-				} else if (livingEntity.getType() == EntityType.SKELETON) {
-					livingEntity.dropStack(new ItemStack(Items.SKELETON_SKULL));
-				} else if (livingEntity.getType() == EntityType.ZOMBIE) {
-					livingEntity.dropStack(new ItemStack(Items.ZOMBIE_HEAD));
-				} else if (livingEntity.getType() == EntityType.CREEPER) {
-					livingEntity.dropStack(new ItemStack(Items.CREEPER_HEAD));
-				} else if (livingEntity.getType() == EntityType.PLAYER) {
-					livingEntity.dropStack(new ItemStack(Items.PLAYER_HEAD));
+			LivingEntity livingEntity = (LivingEntity) (Object) this;
+			if (damageSource.getSource() instanceof LivingEntity && livingEntity.world.getGameRules().getBoolean(GameRules.DO_MOB_LOOT) && BeheadingEnchantment.hasBeheading((LivingEntity) damageSource.getSource())) {
+				if (BeheadingEnchantment.getHead(damageSource)) {
+					if (livingEntity.getType() == EntityType.WITHER_SKELETON) {
+						livingEntity.dropStack(new ItemStack(Items.WITHER_SKELETON_SKULL));
+					} else if (livingEntity.getType() == EntityType.SKELETON) {
+						livingEntity.dropStack(new ItemStack(Items.SKELETON_SKULL));
+					} else if (livingEntity.getType() == EntityType.ZOMBIE) {
+						livingEntity.dropStack(new ItemStack(Items.ZOMBIE_HEAD));
+					} else if (livingEntity.getType() == EntityType.CREEPER) {
+						livingEntity.dropStack(new ItemStack(Items.CREEPER_HEAD));
+					} else if (livingEntity.getType() == EntityType.PLAYER) {
+						livingEntity.dropStack(new ItemStack(Items.PLAYER_HEAD));
+					}
 				}
 			}
-		}
 	}
 
 	@Inject(method = "applyDamage", at = @At("RETURN"))
 	public void applyDamage(DamageSource damageSource, float damage, CallbackInfo info) {
 		LivingEntity attacked = (LivingEntity) (Object) this;
-		if (damageSource.getAttacker() instanceof LivingEntity && attacked.getHealth() < damage) {
-			LivingEntity attacker = (LivingEntity) damageSource.getAttacker();
+		if (damageSource.getSource() instanceof LivingEntity && attacked.getHealth() < damage) {
+			LivingEntity attacker = (LivingEntity) damageSource.getSource();
 			if (!attacked.isInvulnerableTo(damageSource)) {
 				float health = LifestealEnchantment.getLifeWithSteal(damageSource, damage, attacked);
 				if (health != 0) {
