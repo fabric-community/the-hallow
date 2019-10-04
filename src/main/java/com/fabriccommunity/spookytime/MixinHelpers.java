dd18c9d46acd52485d72342e039656cdf9198992
vini2003 <vini.sch@outlook.com>
2019-10-04 16:20:57 -0300

    Fix imports.

--- src/main/java/com/fabriccommunity/spookytime/MixinHelpers.java
+++ src/main/java/com/fabriccommunity/spookytime/MixinHelpers.java
@@ -1,6 +1,5 @@
 package com.fabriccommunity.spookytime;
 
-import com.google.common.hash.HashCode;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
