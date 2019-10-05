2812e8912248a3ff86fcc112e7005a77ede32efe
Draylar <samuelhimes@gmail.com>
2019-10-04 19:32:15 -0500

    actually check for pumpkin foods

--- src/main/java/com/fabriccommunity/spookytime/mixin/PlayerEntityMixin.java
+++ src/main/java/com/fabriccommunity/spookytime/mixin/PlayerEntityMixin.java
@@ -6,7 +6,9 @@ import dev.emi.trinkets.api.TrinketsApi;
 import net.minecraft.entity.player.HungerManager;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.FoodComponent;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -14,8 +16,19 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.ArrayList;
+
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin {
+	private static final ArrayList<Item> PUMPKIN_FOODS = new ArrayList<>();
+
+	static {
+		PUMPKIN_FOODS.add(Items.PUMPKIN_PIE);
+		PUMPKIN_FOODS.add(SpookyItems.BAKED_PUMPKIN_SEEDS);
+		PUMPKIN_FOODS.add(SpookyItems.PUMPKIN_STEW);
+		PUMPKIN_FOODS.add(SpookyItems.PUMPKIN_CANDY);
+	}
+
 	@Shadow protected HungerManager hungerManager;
 
 	@Inject(at = @At(
@@ -29,12 +42,15 @@ public abstract class PlayerEntityMixin {
 
 		ItemStack mainHandStack = trinketPlayer.getStack("hand:ring");
 		ItemStack offHandStack = trinketPlayer.getStack("offhand:ring");
+		Item item = itemStack.getItem();
 
 		if(mainHandStack.getItem().equals(SpookyItems.PUMPKIN_RING) || offHandStack.getItem().equals(SpookyItems.PUMPKIN_RING)) {
-			if (itemStack.getItem().isFood()) {
-				FoodComponent foodComponent = itemStack.getItem().getFoodComponent();
-				int extraHunger = (int) (foodComponent.getHunger() * .25);
-				this.hungerManager.add(extraHunger, 1);
+			if (item.isFood()) {
+				if(PUMPKIN_FOODS.contains(item)) {
+					FoodComponent foodComponent = item.getFoodComponent();
+					int extraHunger = (int) (foodComponent.getHunger() * .25);
+					this.hungerManager.add(extraHunger, 1);
+				}
 			}
 		}
 	}
