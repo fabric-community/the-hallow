e3ed2adc98720541c6d777d2384e7ad07af64109
cplir-c <3486579-cplir-c@users.noreply.gitlab.com>
2019-10-05 19:17:19 -0500

    rewrite carving storage

--- src/main/java/com/fabriccommunity/spookytime/block/entity/CarvablePumpkinBlockEntity.java
+++ src/main/java/com/fabriccommunity/spookytime/block/entity/CarvablePumpkinBlockEntity.java
@@ -1,6 +1,7 @@
 package com.fabriccommunity.spookytime.block.entity;
 
 import com.fabriccommunity.spookytime.common.SpookyBlocks;
+
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -9,13 +10,15 @@ import net.minecraft.item.ShearsItem;
 import net.minecraft.nbt.ByteTag;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.LongTag;
+import net.minecraft.nbt.Tag;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
-
+import java.util.BitSet;
 /**
  * @author Indigo Amann
  */
@@ -23,40 +26,77 @@ public class CarvablePumpkinBlockEntity extends BlockEntity {
     public CarvablePumpkinBlockEntity() {
         super(SpookyBlocks.CARVABLE_PUMPKIN_BLOCK_ENTITY);
     }
-    //                                      Face  X   Y
-    public boolean[][][] carving = new boolean[4][16][16];
+    //                               4 Faces
+    public BitSet[] carved = new BitSet[4];
 
     @Override
     public CompoundTag toTag(CompoundTag tag) {
         super.toTag(tag);
         ListTag faces = new ListTag();
-        for (boolean[][] carvingFace : carving) {
-            ListTag face = new ListTag();
-            for (boolean[] carvingRow: carvingFace) {
-                ListTag row = new ListTag();
-                for (boolean carved: carvingRow) {
-                    row.add(new ByteTag((byte) (carved ? 1 : 0)));
-                }
-                face.add(row);
-            }
-            faces.add(face);
+        for (BitSet carvedFace : carved) {
+        	ListTag face = new ListTag();
+        	
+        	if(carvedFace == null) {
+        		face = new ListTag();
+        	} else {
+	            for (long twoRows: carvedFace.toLongArray()) {
+	                face.add(new LongTag(twoRows));
+	            }
+        	}
+            
+        	faces.add(face);
         }
         tag.put("carving", faces);
         return tag;
     }
+    
+    @Override
+    public void fromTag(CompoundTag tag) {
+    	super.fromTag(tag);
+    	this.carved = new BitSet[4];
+    	ListTag facesTag = tag.getList("carving", new LongTag(0).getType()); 
+    	for (int i = 0; i < 4; i++) {
+    		Tag regularFaceTag = facesTag.get(i);
+    		ListTag faceTag = (ListTag) regularFaceTag;
+    		if (faceTag.isEmpty()) {
+    			this.carved[i] = null;
+    		} else {
+    			long[] face = new long[faceTag.size()];
+    			// maybe emit a warning if the face is not 256 bits long?
+    			for (int j = 0; j < face.length; j++) {
+    				LongTag longTag = (LongTag) faceTag.get(j);
+    				face[j] = longTag.getLong();
+    			}
+    			this.carved[i] = BitSet.valueOf(face);
+    		}
+    	}
+    }
+    
     public boolean activate(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hitResult) {
         if (hitResult.getSide().getAxis() == Direction.Axis.Y) return false;
         Vec3d onBlock = hitResult.getPos().subtract(new Vec3d(hitResult.getBlockPos()));
-        int x = (int) Math.floor(onBlock.x * 16f);
-        int y = (int) Math.floor(onBlock.y * 16f);
-        int z = (int) Math.floor(onBlock.z * 16f);
-        if (carving[hitResult.getSide().getId() - 2][hitResult.getSide().getAxis() == Direction.Axis.Z ? x : z][y]) return false;
+        
+        int x = (int) (onBlock.x * 16f);
+        int y = (int) (onBlock.y * 16f);
+        int z = (int) (onBlock.z * 16f);
+        
+        int face = hitResult.getSide().getId() - 2;
+        int faceX = hitResult.getSide().getAxis() == Direction.Axis.Z ? x : z;
+        
+        if (carved[face] != null && carved[face].get(faceX + 16 * y)) {
+        	return false;
+        }
+        
         ItemStack stack = player.getStackInHand(hand);
         if (stack.getItem() instanceof ShearsItem) {
             if (!player.abilities.creativeMode) {
                 stack.damage(1, player, it -> {});
             }
-            carving[hitResult.getSide().getId() - 2][hitResult.getSide().getAxis() == Direction.Axis.Z ? x : z][y] = true;
+            if (carved[face] == null) {
+            	// Initialize uncarved face
+            	carved[face] = new BitSet(256);
+            }
+            carved[face].set(faceX + 16 * y, true);
             return true;
         }
         return false;
--- src/main/java/com/fabriccommunity/spookytime/client/render/blockentity/CarvablePumpkinBER.java
+++ src/main/java/com/fabriccommunity/spookytime/client/render/blockentity/CarvablePumpkinBER.java
@@ -2,6 +2,8 @@ package com.fabriccommunity.spookytime.client.render.blockentity;
 
 import com.fabriccommunity.spookytime.block.entity.CarvablePumpkinBlockEntity;
 import com.mojang.blaze3d.platform.GlStateManager;
+
+import net.minecraft.block.Blocks;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.BufferBuilder;
 import net.minecraft.client.render.Tessellator;
@@ -14,84 +16,118 @@ import org.lwjgl.opengl.GL11;
 
 /**
  * @author Indigo Amann
+ * @author cplir-c
  */
 public class CarvablePumpkinBER extends BlockEntityRenderer<CarvablePumpkinBlockEntity> {
-    @Override
+    protected void renderTopAndBottom() {
+    	Sprite top = MinecraftClient.getInstance().getSpriteAtlas().getSprite("block/pumpkin_top");
+        drawFace(top, Direction.UP, 0, 0, 16, 16);
+        drawFace(top, Direction.DOWN, 0, 0, 16, 16);
+    }
+    protected void renderTopAndBottomTwice(){
+    	renderTopAndBottom();
+    	Sprite top = MinecraftClient.getInstance().getSpriteAtlas().getSprite("block/pumpkin_top");
+    	drawFace(top, Direction.UP, 0, 16, 16, 0);
+        drawFace(top, Direction.DOWN, 0, 16, 16, 0);
+    }
+	@Override
     public void render(CarvablePumpkinBlockEntity blockEntity, double bx, double by, double bz, float partialTicks, int destroyStage) {
         GlStateManager.pushMatrix();
         GlStateManager.translated(bx, by, bz);
         GlStateManager.scalef(1f / 16f, 1f / 16f, 1f / 16f);
+        
         renderManager.textureManager.bindTexture(SpriteAtlasTexture.BLOCK_ATLAS_TEX);
+        
         GlStateManager.enableTexture();
-        Sprite side = MinecraftClient.getInstance().getSpriteAtlas().getSprite("block/pumpkin_side");
-        {
-            Sprite top = MinecraftClient.getInstance().getSpriteAtlas().getSprite("block/pumpkin_top");
-            drawFace(top, Direction.UP, 0, 0, 16, 16);
-            drawFace(top, Direction.DOWN, 0, 0, 16, 16);
-        }
+        Sprite pumpkinSideSprite = MinecraftClient.getInstance().getSpriteAtlas().getSprite("block/pumpkin_side");
+        
         for (int f = 0; f < 4; f++) {
-            for (int x = 0; x < 16; x++) {
-                for (int y = 0; y < 16; y++) {
-                    if(!blockEntity.carving[f][x][y]) {
-                        //drawFace(side, Direction.byId(f + 2), x + 1, 16 - x, 16 - y, y + 1);
-                        drawFace(side, Direction.byId(f + 2), x, y, x+1, y + 1);
-                    }
+        	if (blockEntity.carved[f] != null) {
+        		// render all sides twice
+        		
+                renderTopAndBottomTwice();
+                for (f = 0; f < 4; f++) {
+                	if(blockEntity.carved[f] == null) {
+                		drawFace(pumpkinSideSprite, Direction.byId(f + 2), 0, 0, 16, 16);
+                		drawFace(pumpkinSideSprite, Direction.byId(f + 2), 0, 16, 16, 0);
+                	} else {
+        	            for (int x = 0; x < 16; x++) {
+        	                for (int y = 0; y < 16; y++) {
+        	                	if(!blockEntity.carved[f].get(16 * x + y)) {
+        	                        drawFace(pumpkinSideSprite, Direction.byId(f + 2), x, y, x + 1, y + 1);
+        	                        drawFace(pumpkinSideSprite, Direction.byId(f + 2), x, y + 1, x + 1, y);
+        	                    }
+        	                }
+        	            }
+                	}
                 }
-            }
+                break;
+        	}
+        	if(f == 3) {
+        		// Render all sides once
+        		renderTopAndBottom();
+        		for (f = 0; f < 4 ; f++) {
+        			drawFace(pumpkinSideSprite, Direction.byId(f + 2), 0, 0, 16, 16);
+        		}
+        	}
         }
-        /*drawFace(side, Direction.NORTH, 0, 0, 16, 16);
-        drawFace(side, Direction.SOUTH, 0, 0, 16, 16);
-        drawFace(side, Direction.EAST, 0, 0, 16, 16);
-        drawFace(side, Direction.WEST, 0, 0, 16, 16);*/
+        
+        
+        
         GlStateManager.popMatrix();
     }
     private void drawFace(Sprite sprite, Direction direction, int x, int z, int mx, int mz) {
         BufferBuilder bufferBuilder = Tessellator.getInstance().getBufferBuilder();
         bufferBuilder.begin(GL11.GL_QUADS, VertexFormats.POSITION_UV);
-        float u = sprite.getU(z), mu = sprite.getU(mz), v = sprite.getV(x), mv = sprite.getV(mx);
+        double u = sprite.getU(z);
+        double mu = sprite.getU(mz);
+        
+        double v = sprite.getV(x);
+        double mv = sprite.getV(mx);
+        
         int y;
         switch (direction) {
             case DOWN:
                 y = 0;
-                bufferBuilder.vertex((double) mx, (double) y, (double) z).texture((double) mu, (double) v).next();
-                bufferBuilder.vertex((double) mx, (double) y, (double) mz).texture((double) u, (double) v).next();
-                bufferBuilder.vertex((double) x, (double) y, (double) mz).texture((double) u, (double) mv).next();
-                bufferBuilder.vertex((double) x, (double) y, (double) z).texture((double) mu, (double) mv).next();
+                bufferBuilder.vertex(mx, y, z) .texture(mu, v) .next();  
+                bufferBuilder.vertex(mx, y, mz).texture(u, v)  .next(); 
+                bufferBuilder.vertex(x, y, mz) .texture(mu, mv).next();
+                bufferBuilder.vertex(x, y, z)  .texture(u, mv) .next();
                 break;
             case UP:
                 y = 16;
-                bufferBuilder.vertex((double) x, (double) y, (double) z).texture((double) u, (double) v).next();
-                bufferBuilder.vertex((double) x, (double) y, (double) mz).texture((double) mu, (double) v).next();
-                bufferBuilder.vertex((double) mx, (double) y, (double) mz).texture((double) mu, (double) mv).next();
-                bufferBuilder.vertex((double) mx, (double) y, (double) z).texture((double) u, (double) mv).next();
+                bufferBuilder.vertex(x, y, z)  .texture(u, v)  .next(); 
+                bufferBuilder.vertex(x, y, mz) .texture(mu, v) .next();
+                bufferBuilder.vertex(mx, y, mz).texture(mu, mv).next(); 
+                bufferBuilder.vertex(mx, y, z) .texture(u, mv) .next();
                 break;
             case NORTH:
                 y = 0;
-                bufferBuilder.vertex((double) z, (double) x, (double) y).texture((double) mu, (double) mv).next();
-                bufferBuilder.vertex((double) z, (double) mx, (double) y).texture((double) mu, (double) v).next();
-                bufferBuilder.vertex((double) mz, (double) mx, (double) y).texture((double) u, (double) v).next();
-                bufferBuilder.vertex((double) mz, (double) x, (double) y).texture((double) u, (double) mv).next();
+                bufferBuilder.vertex(z, x, y)  .texture(u, v)  .next();
+                bufferBuilder.vertex(z, mx, y) .texture(u, mv) .next();
+                bufferBuilder.vertex(mz, mx, y).texture(mu, mv).next();
+                bufferBuilder.vertex(mz, x, y) .texture(mu, v) .next();
                 break;
             case SOUTH:
                 y = 16;
-                bufferBuilder.vertex((double) mz, (double) x, (double) y).texture((double) mu, (double) mv).next();
-                bufferBuilder.vertex((double) mz, (double) mx, (double) y).texture((double) mu, (double) v).next();
-                bufferBuilder.vertex((double) z, (double) mx, (double) y).texture((double) u, (double) v).next();
-                bufferBuilder.vertex((double) z, (double) x, (double) y).texture((double) u, (double) mv).next();
+                bufferBuilder.vertex(mz, x, y) .texture(u, v)  .next();
+                bufferBuilder.vertex(mz, mx, y).texture(u, mv) .next();
+                bufferBuilder.vertex(z, mx, y) .texture(mu, mv).next();
+                bufferBuilder.vertex(z, x, y)  .texture(mu, v) .next();
                 break;
             case WEST:
                 y = 16;
-                bufferBuilder.vertex((double) y, (double) x, (double) z).texture((double) mu, (double) mv).next();
-                bufferBuilder.vertex((double) y, (double) mx, (double) z).texture((double) mu, (double) v).next();
-                bufferBuilder.vertex((double) y, (double) mx, (double) mz).texture((double) u, (double) v).next();
-                bufferBuilder.vertex((double) y, (double) x, (double) mz).texture((double) u, (double) mv).next();
+                bufferBuilder.vertex(y, x, z)  .texture(u, v)  .next();
+                bufferBuilder.vertex(y, mx, z) .texture(u, mv) .next();
+                bufferBuilder.vertex(y, mx, mz).texture(mu, mv).next();
+                bufferBuilder.vertex(y, x, mz) .texture(mu, v) .next();
                 break;
             case EAST:
                 y = 0;
-                bufferBuilder.vertex((double) y, (double) x, (double) mz).texture((double) u, (double) mv).next();
-                bufferBuilder.vertex((double) y, (double) mx, (double) mz).texture((double) u, (double) v).next();
-                bufferBuilder.vertex((double) y, (double) mx, (double) z).texture((double) mu, (double) v).next();
-                bufferBuilder.vertex((double) y, (double) x, (double) z).texture((double) mu, (double) mv).next();
+                bufferBuilder.vertex(y, x, z)  .texture(u, v)  .next();
+                bufferBuilder.vertex(y, mx, z) .texture(u, mv) .next();
+                bufferBuilder.vertex(y, mx, mz).texture(mu, mv).next();
+                bufferBuilder.vertex(y, x, mz) .texture(mu, v) .next();
                 break;
         }
         Tessellator.getInstance().draw();
