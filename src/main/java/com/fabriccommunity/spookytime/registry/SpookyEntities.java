35fbc8b1c8bdc713e5c0c9b4c9035a8141619379
Draylar <samuelhimes@gmail.com>
2019-10-06 21:40:12 -0500

    fix up BE registration in wrong class

--- src/main/java/com/fabriccommunity/spookytime/registry/SpookyEntities.java
+++ src/main/java/com/fabriccommunity/spookytime/registry/SpookyEntities.java
@@ -4,15 +4,12 @@ import com.fabriccommunity.spookytime.SpookyTime;
 import com.fabriccommunity.spookytime.component.CandyComponent;
 import com.fabriccommunity.spookytime.component.CandyComponent.VillagerCandyComponent;
 import com.fabriccommunity.spookytime.entity.PumpcownEntity;
-import com.fabriccommunity.spookytime.entity.SpookyTreasureChestBlockEntity;
-import com.fabriccommunity.spookytime.entity.SpookyTreasureChestEntity;
 import com.fabriccommunity.spookytime.entity.SpookyCactusEntity;
+import com.fabriccommunity.spookytime.entity.SpookyTreasureChestEntity;
 import nerdhub.cardinal.components.api.ComponentRegistry;
 import nerdhub.cardinal.components.api.ComponentType;
 import nerdhub.cardinal.components.api.event.EntityComponentCallback;
 import net.fabricmc.fabric.api.entity.FabricEntityTypeBuilder;
-import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityCategory;
 import net.minecraft.entity.EntityDimensions;
@@ -23,6 +20,9 @@ import net.minecraft.util.registry.Registry;
 public class SpookyEntities {
 	public static ComponentType<CandyComponent> CANDY = ComponentRegistry.INSTANCE.registerIfAbsent(SpookyTime.id("candy"), CandyComponent.class);
 
+	public static EntityType<PumpcownEntity> PUMPCOWN = register("pumpcown", FabricEntityTypeBuilder.create(EntityCategory.CREATURE, PumpcownEntity::new).size(EntityDimensions.fixed(0.9F, 1.4F)).build());
+	public static EntityType<SpookyCactusEntity> SPOOKY_CACTUS = register("spooky_cactus", FabricEntityTypeBuilder.create(EntityCategory.MISC, SpookyCactusEntity::new).size(EntityDimensions.changing(0.9F, 1.0F)).build());;
+
 	public static final EntityType<SpookyTreasureChestEntity> SPOOKY_TREASURE_CHEST = register(
 		"spooky_treasure_chest",
 		FabricEntityTypeBuilder.<SpookyTreasureChestEntity>create(
@@ -31,17 +31,6 @@ public class SpookyEntities {
 		).size(EntityDimensions.fixed(.5f, .5f)).build()
 	);
 
-	public static final BlockEntityType<SpookyTreasureChestBlockEntity> SPOOKY_TREASURE_CHEST_BE = register(
-		"spooky_treasure_chest",
-		BlockEntityType.Builder.create(
-			SpookyTreasureChestBlockEntity::new,
-			SpookyBlocks.SPOOKY_TREASURE_CHEST
-		).build(null)
-	);
-  
-	public static EntityType<PumpcownEntity> PUMPCOWN = register("pumpcown", FabricEntityTypeBuilder.create(EntityCategory.CREATURE, PumpcownEntity::new).size(EntityDimensions.fixed(0.9F, 1.4F)).build());
-	public static EntityType<SpookyCactusEntity> SPOOKY_CACTUS = register("spooky_cactus", FabricEntityTypeBuilder.create(EntityCategory.MISC, SpookyCactusEntity::new).size(EntityDimensions.changing(0.9F, 1.0F)).build());;
-	
 	private SpookyEntities() {
 		// NO-OP
 	}
