02d5dd2fc78c49cad5a9c7ed9b6e39b29c0752ed
Draylar <samuelhimes@gmail.com>
2019-10-06 21:40:36 -0500

    fix bad field reference

--- src/main/java/com/fabriccommunity/spookytime/entity/SpookyTreasureChestBlockEntity.java
+++ src/main/java/com/fabriccommunity/spookytime/entity/SpookyTreasureChestBlockEntity.java
@@ -1,11 +1,11 @@
 package com.fabriccommunity.spookytime.entity;
 
-import com.fabriccommunity.spookytime.registry.SpookyEntities;
+import com.fabriccommunity.spookytime.registry.SpookyBlockEntities;
 import net.minecraft.block.entity.BlockEntity;
 
 public class SpookyTreasureChestBlockEntity extends BlockEntity {
 
 	public SpookyTreasureChestBlockEntity() {
-		super(SpookyEntities.SPOOKY_TREASURE_CHEST_BE);
+		super(SpookyBlockEntities.SPOOKY_TREASURE_CHEST_BE);
 	}
 }
