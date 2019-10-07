06176dd3747665c4910a5983463134d1293beb65
Will Toll <will@jtoll.com>
2019-10-07 14:56:48 -0600

    Witches with water breathing are not damaged by water

--- src/main/java/com/fabriccommunity/spookytime/mixin/WitchEntityMixin.java
+++ src/main/java/com/fabriccommunity/spookytime/mixin/WitchEntityMixin.java
@@ -1,23 +1,12 @@
 package com.fabriccommunity.spookytime.mixin;
 
-import com.fabriccommunity.spookytime.MixinHelpers;
 import com.fabriccommunity.spookytime.event.WitchTickCallback;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.mob.WitchEntity;
-import net.minecraft.entity.mob.ZombieEntity;
-import net.minecraft.entity.raid.RaiderEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import javax.swing.*;
 
 /**
  * Causes witches to melt (get slowly damaged) while in water
--- src/main/java/com/fabriccommunity/spookytime/registry/SpookyEvents.java
+++ src/main/java/com/fabriccommunity/spookytime/registry/SpookyEvents.java
@@ -1,19 +1,32 @@
 package com.fabriccommunity.spookytime.registry;
 
 import com.fabriccommunity.spookytime.event.WitchTickCallback;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.attribute.EntityAttributeInstance;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionUtil;
+import net.minecraft.potion.Potions;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.tag.FluidTags;
 import net.minecraft.util.ActionResult;
 
 public class SpookyEvents {
 	public static void init() {
 		WitchTickCallback.EVENT.register((witch) -> {
-			if (witch.getEntityWorld().hasRain(witch.getBlockPos().up())) {
-				witch.damage(DamageSource.DROWN, 1.0F);
-				witch.setDrinking(false);
-			} else if (witch.checkWaterState() && !witch.updateMovementInFluid(SpookyFluidTags.WITCH_WATER)) {
-				witch.damage(DamageSource.DROWN, 1.0F);
-				witch.setDrinking(false);
+			if (!witch.hasStatusEffect(StatusEffects.WATER_BREATHING)) {
+				if (witch.getEntityWorld().hasRain(witch.getBlockPos().up())) {
+					witch.damage(DamageSource.DROWN, 4.0F);
+				} else if (witch.checkWaterState() && !witch.updateMovementInFluid(SpookyFluidTags.WITCH_WATER)) {
+					witch.damage(DamageSource.DROWN, 4.0F);
+				}
 			}
 			return ActionResult.PASS;
 		});
