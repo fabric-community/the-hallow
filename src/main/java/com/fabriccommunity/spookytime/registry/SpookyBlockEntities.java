6bf164143c86adef59c4292f1fbf7b21fcee11d5
Grondag <grondag@gmail.com>
2019-10-14 01:56:28 -0700

    Remove cruft, inadvertent changes

--- src/main/java/com/fabriccommunity/spookytime/registry/SpookyBlockEntities.java
+++ src/main/java/com/fabriccommunity/spookytime/registry/SpookyBlockEntities.java
@@ -1,6 +1,9 @@
 package com.fabriccommunity.spookytime.registry;
 
-import java.util.function.Supplier;
+import net.minecraft.block.Block;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.util.registry.Registry;
 
 import com.fabriccommunity.spookytime.SpookyTime;
 import com.fabriccommunity.spookytime.block.entity.InfusionAltarBlockEntity;
@@ -8,10 +11,7 @@ import com.fabriccommunity.spookytime.block.entity.InfusionPillarBlockEntity;
 import com.fabriccommunity.spookytime.block.entity.TinyPumpkinBlockEntity;
 import com.fabriccommunity.spookytime.entity.SpookyTreasureChestBlockEntity;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.block.entity.BlockEntityType;
-import net.minecraft.util.registry.Registry;
+import java.util.function.Supplier;
 
 import static com.fabriccommunity.spookytime.registry.SpookyBlocks.INFUSION_ALTAR_BLOCK;
 import static com.fabriccommunity.spookytime.registry.SpookyBlocks.INFUSION_PILLAR_BLOCK;
@@ -23,33 +23,33 @@ public class SpookyBlockEntities {
 		SpookyBlocks.TINY_PUMPKIN,
 		SpookyBlocks.WITCHED_PUMPKIN
 	);
-
+	
 	public static final BlockEntityType<SpookyTreasureChestBlockEntity> SPOOKY_TREASURE_CHEST_BE = register(
 		"spooky_treasure_chest",
 		SpookyTreasureChestBlockEntity::new,
 		SpookyBlocks.SPOOKY_TREASURE_CHEST
 	);
-
+	
 	public static final BlockEntityType<InfusionPillarBlockEntity> INFUSION_PILLAR_BLOCK_ENTITY = register(
 		"infusion_pillar",
 		InfusionPillarBlockEntity::new,
 		INFUSION_PILLAR_BLOCK
 	);
-
+	
 	public static final BlockEntityType<InfusionAltarBlockEntity> INFUSION_ALTAR_BLOCK_ENTITY = register(
 		"infusion_altar",
 		InfusionAltarBlockEntity::new,
 		INFUSION_ALTAR_BLOCK
 	);
-
+	
 	private SpookyBlockEntities() {
 		// NO-OP
 	}
-
+	
 	public static void init() {
 		// NO-OP
 	}
-
+	
 	private static <B extends BlockEntity> BlockEntityType<B> register(String name, Supplier<B> supplier, Block... supportedBlocks) {
 		return Registry.register(Registry.BLOCK_ENTITY, SpookyTime.id(name), BlockEntityType.Builder.create(supplier, supportedBlocks).build(null));
 	}
--- src/main/java/com/fabriccommunity/spookytime/registry/SpookyTags.java
+++ src/main/java/com/fabriccommunity/spookytime/registry/SpookyTags.java
@@ -1,22 +1,23 @@
 package com.fabriccommunity.spookytime.registry;
 
-import com.fabriccommunity.spookytime.SpookyTime;
-
 import net.fabricmc.fabric.api.tag.TagRegistry;
+
 import net.minecraft.item.Item;
 import net.minecraft.tag.Tag;
 
+import com.fabriccommunity.spookytime.SpookyTime;
+
 public class SpookyTags {
 	public static final Tag<Item> COSTUMES = register("costumes");
-
+	
 	private SpookyTags() {
 		// NO-OP
 	}
-
+	
 	public static void init() {
 		// NO-OP
 	}
-
+	
 	public static Tag<Item> register(String name) {
 		return TagRegistry.item(SpookyTime.id(name));
 	}
