bcee9ee665c3111bdbbafeaae672d89685747deb
vronc <veronica__94@hotmail.com>
2019-10-14 10:50:24 +0200

    Add basic tombstone object with texture. NOT DONE.

---
+++ src/main/java/com/fabriccommunity/spookytime/block/TombstoneBlock.java
@@ -0,0 +1,48 @@
+package com.fabriccommunity.spookytime.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockPlacementEnvironment;
+import net.minecraft.block.BlockRenderLayer;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.EntityContext;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.ViewableWorld;
+
+public class TombstoneBlock extends Block {
+	private static final VoxelShape SHAPE = Block.createCuboidShape(3.0D, 0.0D, 3.0D, 13.0D, 15.0D, 6.0D);
+
+	public TombstoneBlock(Settings settings) {
+		super(settings);
+	}
+
+	@Override
+	public boolean canPlaceAtSide(BlockState state, BlockView blockView, BlockPos pos, BlockPlacementEnvironment bpe) {
+		return bpe == BlockPlacementEnvironment.LAND;
+	}
+
+	@Override
+	public VoxelShape getOutlineShape(BlockState state, BlockView blockView, BlockPos pos, EntityContext context) {
+		return SHAPE;
+	}
+
+	@Override
+	public boolean canPlaceAt(BlockState state, ViewableWorld world, BlockPos pos) {
+		BlockPos down = pos.down();
+		return Block.isFaceFullSquare(world.getBlockState(down).getCollisionShape(world, down), Direction.UP);
+	}
+
+	@Override
+	public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState state2, IWorld world, BlockPos pos, BlockPos pos2) {
+		return !state.canPlaceAt(world, pos) ? Blocks.AIR.getDefaultState() : super.getStateForNeighborUpdate(state, direction, state2, world, pos, pos2);
+	}
+
+	@Override
+	public BlockRenderLayer getRenderLayer() {
+		return BlockRenderLayer.CUTOUT;
+	}
+}
--- src/main/java/com/fabriccommunity/spookytime/registry/SpookyBlocks.java
+++ src/main/java/com/fabriccommunity/spookytime/registry/SpookyBlocks.java
@@ -1,5 +1,6 @@
 package com.fabriccommunity.spookytime.registry;
 
+import com.fabriccommunity.spookytime.block.*;
 import net.fabricmc.fabric.api.block.FabricBlockSettings;
 
 import net.minecraft.block.Block;
@@ -21,19 +22,6 @@ import net.minecraft.util.Rarity;
 import net.minecraft.util.registry.Registry;
 
 import com.fabriccommunity.spookytime.SpookyTime;
-import com.fabriccommunity.spookytime.block.BloodBlock;
-import com.fabriccommunity.spookytime.block.BreadCrumbsBlock;
-import com.fabriccommunity.spookytime.block.DeaderBushBlock;
-import com.fabriccommunity.spookytime.block.DeceasedGrassBlock;
-import com.fabriccommunity.spookytime.block.InfusionAltarBlock;
-import com.fabriccommunity.spookytime.block.InfusionPillarBlock;
-import com.fabriccommunity.spookytime.block.SpookyCactusBlock;
-import com.fabriccommunity.spookytime.block.SpookyStairsBlock;
-import com.fabriccommunity.spookytime.block.SpookyTreasureChestBlock;
-import com.fabriccommunity.spookytime.block.TinyPumpkinBlock;
-import com.fabriccommunity.spookytime.block.TranslucentGlassBlock;
-import com.fabriccommunity.spookytime.block.TranslucentGlassPaneBlock;
-import com.fabriccommunity.spookytime.block.WitchWaterBlock;
 import com.fabriccommunity.spookytime.item.WitchedPumpkinItem;
 
 import java.util.function.Function;
@@ -87,6 +75,7 @@ public class SpookyBlocks {
 	public static final Block SPOOKY_CACTUS = register("spooky_cactus", new SpookyCactusBlock(FabricBlockSettings.copy(Blocks.CACTUS).materialColor(MaterialColor.BROWN).build()), new Item.Settings().group(SpookyTime.GROUP));
 	public static final Block DEADER_BUSH = register("deader_bush", new DeaderBushBlock(FabricBlockSettings.copy(Blocks.DEAD_BUSH).materialColor(MaterialColor.BROWN).build()), new Item.Settings().group(SpookyTime.GROUP));
 	public static final Block BREAD_CRUMBS = register("bread_crumbs", new BreadCrumbsBlock(FabricBlockSettings.of(Material.CAKE).breakByHand(true).collidable(false).noCollision().build()), new Item.Settings().group(SpookyTime.GROUP).food(new FoodComponent.Builder().hunger(1).saturationModifier(0.1f).snack().build()));
+	public static final Block TOMBSTONE = register("tombstone", new TombstoneBlock(FabricBlockSettings.of(Material.CARPET).breakByHand(true).collidable(false).noCollision().build()), new Item.Settings().group(SpookyTime.GROUP).food(new FoodComponent.Builder().hunger(-1).saturationModifier(0.1f).snack().build()));
 	public static final Block INFUSION_PILLAR_BLOCK = register("infusion_pillar", new InfusionPillarBlock(FabricBlockSettings.copy(Blocks.COBBLESTONE_WALL).build()));
 	public static final Block INFUSION_ALTAR_BLOCK = register("infusion_altar", new InfusionAltarBlock(FabricBlockSettings.copy(Blocks.COBBLESTONE_WALL).build()));
 	public static final Block MOIST_FLESH_BLOCK = register("moist_flesh_block", new Block(FabricBlockSettings.copy(Blocks.NETHER_WART_BLOCK).materialColor(MaterialColor.PINK).sounds(BlockSoundGroup.SLIME).build()));
