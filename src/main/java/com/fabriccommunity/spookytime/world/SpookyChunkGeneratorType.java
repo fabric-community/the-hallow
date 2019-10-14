63f34565cb726bcb6a2dc31da4fde531a7a47199
Jacob Farley <jacobfarley2017@gmail.com>
2019-10-13 22:09:55 -0500

    Do not register our custom ChunkGeneratorType. This fixes server-side compat without seemingly any issues. I don't think ChunkGeneratorType really needs to be synced in the first place.

--- src/main/java/com/fabriccommunity/spookytime/world/SpookyChunkGeneratorType.java
+++ src/main/java/com/fabriccommunity/spookytime/world/SpookyChunkGeneratorType.java
@@ -1,17 +1,14 @@
 package com.fabriccommunity.spookytime.world;
 
-import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.source.BiomeSource;
 import net.minecraft.world.gen.chunk.ChunkGeneratorType;
-
-import com.fabriccommunity.spookytime.SpookyTime;
 
 import java.util.function.Supplier;
 
 public class SpookyChunkGeneratorType extends ChunkGeneratorType<SpookyChunkGeneratorConfig, SpookyChunkGenerator> {
 	
-	public static final SpookyChunkGeneratorType INSTANCE = Registry.register(Registry.CHUNK_GENERATOR_TYPE, SpookyTime.id("spooky"), new SpookyChunkGeneratorType(false, () -> new SpookyChunkGeneratorConfig()));
+	public static final SpookyChunkGeneratorType INSTANCE = new SpookyChunkGeneratorType(false, () -> new SpookyChunkGeneratorConfig());
 	
 	public SpookyChunkGeneratorType(boolean buffetScreen, Supplier<SpookyChunkGeneratorConfig> configSupplier) {
 		super(null, buffetScreen, configSupplier);
