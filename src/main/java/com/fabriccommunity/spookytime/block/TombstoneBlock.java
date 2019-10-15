6bfd292d04cd2b3b18d47367b861f024dea6c7e6
vronc <veronica__94@hotmail.com>
2019-10-15 13:59:15 +0200

    Fixed placement according to player position.

--- src/main/java/com/fabriccommunity/spookytime/block/TombstoneBlock.java
+++ src/main/java/com/fabriccommunity/spookytime/block/TombstoneBlock.java
@@ -1,48 +1,50 @@
 package com.fabriccommunity.spookytime.block;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockPlacementEnvironment;
-import net.minecraft.block.BlockRenderLayer;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.*;
 import net.minecraft.entity.EntityContext;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateFactory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
-import net.minecraft.world.IWorld;
-import net.minecraft.world.ViewableWorld;
 
-public class TombstoneBlock extends Block {
-	private static final VoxelShape SHAPE = Block.createCuboidShape(3.0D, 0.0D, 10.0D, 13.0D, 15.0D, 13.0D);
+import java.util.AbstractMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class TombstoneBlock extends HorizontalFacingBlock {
+	private static final Map<Direction, VoxelShape> SHAPES = Stream.of(
+		new AbstractMap.SimpleEntry<>(Direction.NORTH, Block.createCuboidShape(3.0D, 0.0D, 10.0D, 13.0D, 15.0D, 13.0D)),
+		new AbstractMap.SimpleEntry<>(Direction.SOUTH, Block.createCuboidShape(3.0D, 0.0D, 3.0D, 13.0D, 15.0D, 6.0D)),
+		new AbstractMap.SimpleEntry<>(Direction.EAST, Block.createCuboidShape(3.0D, 0.0D, 3.0D, 6.0D, 15.0D, 13.0D)),
+		new AbstractMap.SimpleEntry<>(Direction.WEST, Block.createCuboidShape(10.0D, 0.0D, 3.0D, 13.0D, 15.0D, 13.0D))
+		)
+		.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
 
 	public TombstoneBlock(Settings settings) {
 		super(settings);
 	}
 
 	@Override
-	public boolean canPlaceAtSide(BlockState state, BlockView blockView, BlockPos pos, BlockPlacementEnvironment bpe) {
-		return bpe == BlockPlacementEnvironment.LAND;
+	public BlockState getPlacementState(ItemPlacementContext placementContext) {
+
+		return this.getDefaultState().with(FACING, placementContext.getPlayerFacing().getOpposite());
+	}
+
+	@Override
+	protected void appendProperties(StateFactory.Builder<Block, BlockState> builder) {
+		builder.add(HorizontalFacingBlock.FACING);
 	}
 
 	@Override
 	public VoxelShape getOutlineShape(BlockState state, BlockView blockView, BlockPos pos, EntityContext context) {
-		return SHAPE;
+		return SHAPES.get(state.get(FACING));
 	}
 
 	@Override
-	public boolean canPlaceAt(BlockState state, ViewableWorld world, BlockPos pos) {
-		BlockPos down = pos.down();
-		return Block.isFaceFullSquare(world.getBlockState(down).getCollisionShape(world, down), Direction.UP);
-	}
-
-	@Override
-	public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState state2, IWorld world, BlockPos pos, BlockPos pos2) {
-		return !state.canPlaceAt(world, pos) ? Blocks.AIR.getDefaultState() : super.getStateForNeighborUpdate(state, direction, state2, world, pos, pos2);
-	}
-
-	@Override
-	public BlockRenderLayer getRenderLayer() {
-		return BlockRenderLayer.CUTOUT;
+	public VoxelShape getCollisionShape(BlockState state, BlockView blockView, BlockPos blockPos, EntityContext entityContext) {
+		return SHAPES.get(state.get(FACING));
 	}
 }
