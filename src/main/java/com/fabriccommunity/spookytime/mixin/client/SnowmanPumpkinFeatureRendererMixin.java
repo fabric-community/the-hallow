0aa597838ced68a4b4ec5756e92b36f09780a80d
i509vcb <30619168+i509VCB@users.noreply.github.com>
2019-10-17 16:10:55 -0500

    Fix improperly named tags

--- src/main/java/com/fabriccommunity/spookytime/mixin/client/SnowmanPumpkinFeatureRendererMixin.java
+++ src/main/java/com/fabriccommunity/spookytime/mixin/client/SnowmanPumpkinFeatureRendererMixin.java
@@ -23,11 +23,9 @@ import net.minecraft.item.ItemStack;
 @Environment(EnvType.CLIENT)
 @Mixin(SnowmanPumpkinFeatureRenderer.class)
 public class SnowmanPumpkinFeatureRendererMixin {
-	
 	// renderPumpkin
 	@Inject(at = @At(value = "INVOKE", target = "net/minecraft/client/render/FirstPersonRenderer.renderItem(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformation$Type;)V"), method = "method_4201(Lnet/minecraft/entity/passive/SnowGolemEntity;FFFFFFF)V", cancellable = true)
-	private void method_4201(SnowGolemEntity snowGolemEntity, float float_1, float float_2, float float_3, float float_4, float float_5, float float_6, float float_7, CallbackInfo ci) {
-
+	private void method_4201(SnowGolemEntity snowGolemEntity, float float1unused, float float2unusued, float float3unusued, float float4unusued, float float5unused, float float6unused, float float7unused, CallbackInfo ci) {
 		Optional<BlockState> blockStateHead = ((SnowGolemEntityModifiers) snowGolemEntity).getHeadState();
 		
 		if(!blockStateHead.isPresent()) {
