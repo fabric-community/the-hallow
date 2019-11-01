254528e9c0d619d2699f012b30fd72640f9b2fb8
Virtuoel <Virtuoel@users.noreply.github.com>
2019-11-02 02:20:04 +0300

    Clean up pie item replacement

--- src/main/java/com/fabriccommunity/thehallow/registry/MinecraftItems.java
+++ src/main/java/com/fabriccommunity/thehallow/registry/MinecraftItems.java
@@ -7,24 +7,20 @@ import net.minecraft.item.FoodComponents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Items;
+import net.minecraft.util.registry.MutableRegistry;
 import net.minecraft.util.registry.Registry;
 
-/*
+/**
  * This registry class is used to overwrite existing registry entries, here vanilla items
- * First parameter of 'register()' method (int) is the raw id in the specified registry
  */
-
 public class MinecraftItems {
-	
-	public static Item PUMPKIN_PIE;
-	
 	public static void init() {
 		if (HallowedConfig.Tweaks.pumpkinPieBlock) {
-			PUMPKIN_PIE = register(Item.getRawId(Items.PUMPKIN_PIE), "pumpkin_pie", new BlockItem(HallowedBlocks.PUMPKIN_PIE, new Item.Settings().group(ItemGroup.FOOD).food(FoodComponents.PUMPKIN_PIE)));
+			replaceEntry(Registry.ITEM, Items.PUMPKIN_PIE, new BlockItem(HallowedBlocks.PUMPKIN_PIE, new Item.Settings().group(ItemGroup.FOOD).food(FoodComponents.PUMPKIN_PIE)));
 		}
 	}
 	
-	private static Item register(int rawId, String name, Item item) {
-		return Registry.register(Registry.ITEM, rawId, name, item);
+	private static <T> T replaceEntry(MutableRegistry<T> registry, T oldEntry, T newEntry) {
+		return registry.set(registry.getRawId(oldEntry), registry.getId(oldEntry), newEntry);
 	}
 }
