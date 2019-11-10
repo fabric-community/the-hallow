5d6ed7e713415b0d12e6c36c02753fd36a3da230
Virtuoel <Virtuoel@users.noreply.github.com>
2019-11-10 15:35:07 +0300

    Remove redundant item class

--- src/main/java/com/fabriccommunity/thehallow/item/ShotgunShellItem.java
+++
@@ -1,9 +0,0 @@
-package com.fabriccommunity.thehallow.item;
-
-import net.minecraft.item.Item;
-
-public class ShotgunShellItem extends Item {
-	public ShotgunShellItem(Settings itemSettings) {
-		super(itemSettings);
-	}
-}
